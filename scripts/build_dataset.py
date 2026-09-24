7948db1fab7e37bca096df7714fdf223fccd13a1
Joyprakash Hela <jayhela3@gmail.com>
2026-09-24 18:19:30 +0530

    Made changes in scripts

--- scripts/build_dataset.py
+++ scripts/build_dataset.py
@@ -2,34 +2,7 @@ from pathlib import Path
 from preprocessing.find_videos import find_videos
 from preprocessing.split_videos import split_videos
 from preprocessing.extract_frames import build_zip
-
-DATASET_DIR = Path("/kaggle/input/datasets/xdxd003/ff-c23/FaceForensics++_C23")
-
-ORIGINAL_DIR = DATASET_DIR/"original"
-
-FAKE_DIRS = [
-    DATASET_DIR/"DeepFakeDetection",
-    DATASET_DIR/"Deepfakes",
-    DATASET_DIR/"Face2Face",
-    DATASET_DIR/"FaceShifter",
-    DATASET_DIR/"FaceSwap",
-    DATASET_DIR/"NeuralTextures"
-
-]
-
-
-SEED = 42
-FRAMES_PER_VIDEO = 10
-
-TRAIN_RATIO = 0.70
-VAL_RATIO = 0.15
-
-OUTPUT_DIR = Path("/kaggle/working/deepfake_frames")
-
-JPEG_QUALITY = 95
-WORKERS = 8
-
-RESIZE = None
+from preprocessing.config import DATASET_DIR, ORIGINAL_DIR, FAKE_DIRS, SEED, FRAMES_PER_VIDEO, TRAIN_RATIO, VAL_RATIO, TEST_RATIO, OUTPUT_DIR, JPEG_QUALITY, WORKERS, RESIZE
 
 real_videos = find_videos(ORIGINAL_DIR)
 
@@ -48,6 +21,7 @@ print("\nREAL")
 print("Train: ", len(real_train))
 print("Validation: ", len(real_val))
 print("Test: ", len(real_test))
+
 
 print("\nFAKE")
 print("Train: ", len(fake_train))
